2ebbc401a617f462a0185348d273d934f5c4da6f
Usama Adeel <usamaadeel1183@gmail.com>
2025-07-16 23:50:41 +0500

    change

--- app/page.js
+++ app/page.js
@@ -9,13 +9,13 @@ export default function LoginPage() {
   useEffect(() => {
     new Swiper('.swiper', {
       loop: true,
-     
-      slidesPerView: 3,
+      centeredSlides: true,
+      slidesPerView: 2.5,
       spaceBetween: 20,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+      // autoplay: {
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -64,58 +64,52 @@ export default function LoginPage() {
       </div>
 
       {/* Right Panel */}
-      <div className="w-full lg:w-1/2 bg-[#0070f3] text-white flex flex-col justify-center items-center text-center py-16 px-4">
+      <div className="w-full lg:w-1/2 bg-[#0070f3] text-white flex flex-col justify-center items-center text-center py-16">
         <h1 className="text-[28px] sm:text-[32px] lg:text-[54px] leading-[34px] sm:leading-[38px] lg:leading-[60px] font-bold mb-6">
           Proof That Our <br /> Software Works
         </h1>
 
         {/* Swiper */}
         <div className="swiper w-full max-w-full py-2 lg:py-2 mb-5">
-          <div className="swiper-wrapper flex items-center h-[300px]">
-            <div className="swiper-slide rounded-[20px]">
+          <div className="swiper-wrapper scale-[1.0] lg:scale-[1.3] flex items-center">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-1.png"
                 alt="Slide 1"
               />
             </div>
-            <div className="swiper-slide rounded-[20px]">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-2.png"
                 alt="Slide 2"
               />
             </div>
-            <div className="swiper-slide rounded-[20px]">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-3.png"
                 alt="Slide 3"
               />
             </div>
-            <div className="swiper-slide rounded-[20px]">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-1.png"
                 alt="Slide 1"
               />
             </div>
-            <div className="swiper-slide rounded-[20px]">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-2.png"
                 alt="Slide 2"
               />
             </div>
-            <div className="swiper-slide rounded-[20px]">
+            <div className="swiper-slide">
               <img
-              
                 src="img/slide-3.png"
                 alt="Slide 3"
               />
             </div>
           </div>
-          <div className="swiper-pagination mt-5 text-white" />
+          <div className="swiper-pagination mt-2 !bottom-[-10px]" />
         </div>
 
         <p className="text-[14px] sm:text-[16px] lg:text-[20px] mb-3 px-2">
@@ -127,3 +121,4 @@ export default function LoginPage() {
     </div>
   );
 }
+
